83dc79c004dc0556ec4d04777dca468b621c8354
brett <brett@debian-laptop>
2016-10-23 19:00:39 -0700

    Bumped version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt') as inf:
 
 setup(
     name='aquatic-twitter',
-    version='0.1.4',
+    version='0.1.5',
     description='Twitter client for various projects',
     long_description=long_description,
     author='Brett Smythe',
